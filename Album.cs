d4c3ab188101520f18e9666173414b43398f9928
Austin Pierce <Austinentpierce@gmail.com>
2021-08-13 23:33:04 -0400

    finished filling in album class

--- Album.cs
+++ Album.cs
@@ -3,6 +3,10 @@ namespace RecordLabelDB
     public class Album
     {
         public int Id { get; set; }
+        public string Title { get; set; }
+        public bool IsExplicit { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public Band Band { get; set; }
 
     }
 }
